29a9c67498ed058d7d8f149b169e5f7ca5fdac1c
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-11-28 17:27:30 +0100

    new files, css and start card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main"
+import Footer from "./components/Footer/Footer"; 
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <div>
-      hello world
-     </div>
+     <Header />
+     <Main />
+     <Footer />
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+export default function(){
+    return  <main>
+        <div className="container">
+            <div className="card">
+                <div className="foto"></div>
+                <div className="content">
+                    
+                </div>
+            </div>    
+        </div> 
+    </main>
+}
